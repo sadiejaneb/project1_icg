4f6ef73b6b7096b9b23a9caec221f21b52724f9f
sadiejaneb <sadiejaneb@gmail.com>
2023-09-10 19:01:27 -0400

    added coin counter scripts and ui elements

--- Assets/Scripts/PlayerInventory.cs
+++ Assets/Scripts/PlayerInventory.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlayerInventory : MonoBehaviour
 {
    public int NumberofCoins { get; private set;}
 
+   public UnityEvent<PlayerInventory> OnCoinCollected;
+
     public void AddCoins()
     {
          NumberofCoins++;
+         OnCoinCollected.Invoke(this);
     }
 
 }
